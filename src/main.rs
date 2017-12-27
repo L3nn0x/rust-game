066aa06866d3d0c7d23e880677b2ac32b48ae0a0
L3nn0x <dragon.super@laposte.net>
2017-12-27 11:55:47 +0000

    installed sdl2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate sdl2;
+
 fn main() {
-    println!("Hello, world!");
+    let ctx = sdl2::init().unwrap();
+    let video_ctx = ctx.video().unwrap();
+    let mut timer = ctx.timer().unwrap();
+
+    let mut window = match video_ctx.window("test", 640, 480).position_centered().opengl().build() {
+        Ok(window) => window,
+        Err(err) => panic!("Failed to create window: {}", err)
+    };
+
+    window.show();
+    timer.delay(3000);
 }
