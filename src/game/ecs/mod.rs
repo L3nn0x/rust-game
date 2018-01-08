288ecf526475c8626436e9d0ed382070b720faa7
L3nn0x <dragon83.super@gmail.com>
2018-01-08 11:52:00 +0000

    Update mod.rs

--- src/game/ecs/mod.rs
+++ src/game/ecs/mod.rs
@@ -10,7 +10,25 @@ use self::specs::{World, DispatcherBuilder, Dispatcher};
 
 pub struct DeltaTime(pub u64);
 
-pub type Command = fn(specs::Entity);
+pub type Player = specs::Entity;
+
+pub mod Commands {
+    use super::*;
+    
+    pub type Command = fn();
+    
+    pub fn move_up(entity: &specs::Entity) {
+    }
+    
+    pub fn move_down(entity: &specs::Entity) {
+    }
+    
+    pub fn move_left(entity: &specs::Entity) {
+    }
+    
+    pub fn move_right(entity: &specs::Entity) {
+    }
+}
 
 pub fn build_world() -> World {
     let mut world = World::new();
@@ -33,7 +51,7 @@ pub fn display(world: &World, canvas: &mut sdl2::render::WindowCanvas) {
     display.run_now(&world.res);
 }
 
-pub fn create_player(world: &mut World) -> specs::Entity {
+pub fn create_player(world: &mut World) -> Player {
     world.create_entity().with(Position{x: 10, y: 10})
                          .with(Display{rec: sdl2::rect::Rect::new(0, 0, 10, 10), color: sdl2::pixels::Color::RGB(255, 0, 0)})
                          .build()
